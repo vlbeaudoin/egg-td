478f84a1501fce362e19a3d9a1b0ed1d22c71d51
Victor Lacasse-Beaudoin <vlbeaudoin@users.noreply.github.com>
2020-12-15 14:48:50 -0500

    Add cell_id and cell_name at cursor in debug_label

--- scripts/DebugLabel.gd
+++ scripts/DebugLabel.gd
@@ -15,6 +15,7 @@ var dynamic_font_size = 6
 
 onready var chickens = get_tree().get_nodes_in_group("chicken")
 onready var mobs = get_tree().get_nodes_in_group("mobs")
+onready var tilemap = $"/root/main/tilemap_buildings" as TileMap
 
 func ready_fonts():
 #	dynamic_font.font_data = load("res://fonts/Retro Gaming.ttf")
@@ -35,6 +36,33 @@ func process_debug_label():
 func update_debug_message():
 	debug_message = ""
 
+	# Cell at cursor
+	var cursor_pos = tilemap.world_to_map(get_viewport().get_mouse_position())
+	var cell_id = tilemap.get_cell(cursor_pos.x, cursor_pos.y)
+	var cell_name: String
+	
+	match cell_id:
+		-1:
+			cell_name = "Empty"
+		0:
+			cell_name = "Grass"
+		1:
+			cell_name = "fence"
+		2:
+			cell_name = "dirt"
+		3:
+			cell_name = "platform_base"
+#			cell_name = "tower"
+		4:
+			cell_name = "chicken-placeholder"
+			
+	debug_message += \
+		"""
+		Cell_id at cursor: %s
+		Cell_name at cursor: %s
+		""" % [cell_id, cell_name]
+
+	# Chickens
 	for chicken in chickens:
 		debug_message += \
 			"""
@@ -45,13 +73,23 @@ func update_debug_message():
 			""" % [chicken, chicken.get_state(), chicken.get_target(), \
 				chicken.get_detected()]
 	
-	for mob in mobs:
-		if mob != null:
-			debug_message += \
-				"""
-				Mob: %s
-				Mob distance to destination: %s
-				""" % [mob, mob.distance]
+#	for mob in mobs:
+#		if mob != null:
+#			debug_message += \
+#				"""
+#				Mob: %s
+#				Mob distance to destination: %s
+#				""" % [mob, mob.distance]
+	
+
+#		"Cell name at curso"
+	
+#	match cell_id:
+#		-1:
+#			cell_to_tilename = "Air"
+		
+		
+	
 	
 	
 ## SETGET

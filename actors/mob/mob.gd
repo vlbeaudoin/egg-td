d807040fb263e7ed321123a11603f7ecc8d80762
Victor Lacasse-Beaudoin <vlbeaudoin@users.noreply.github.com>
2020-12-17 22:39:15 -0500

    Set mobs to flip when looking the other way

--- actors/mob/mob.gd
+++ actors/mob/mob.gd
@@ -4,24 +4,22 @@ extends KinematicBody2D
 ## VARS
 export var mob_type = "base"
 
+# References
+onready var screen_size = get_viewport_rect().size
 onready var mob_navigation = $mob_navigation as Node2D
 onready var health_bar = $health_bar as TextureProgress
-#onready var astar_nav = $astar_nav
 onready var astar_nav = $"/root/main/astar_nav" as AStarPath
+onready var player_buildings = $"/root/main/player_buildings" as TileMap
+onready var sprite = $Sprite as Sprite
+onready var collision_shape = $CollisionShape2D as CollisionShape2D
 
-
-# Movement
-#var velocity = Vector2()
+export(int) var max_health = 3
+export(int) var health = max_health
 export(float) var speed = 30
 var distance setget , get_distance
 
-# Stats
-export(int) var max_health = 3
-export(int) var health = max_health
 
-# References
-onready var screen_size = get_viewport_rect().size
-
+#var looking_left = false
 
 ## FUNCS
 func clamp_position():
@@ -56,4 +54,18 @@ func _ready():
 
 func _process(_delta):
 	health_bar.value = health
+	
+	if mob_navigation and mob_navigation.path:
+		var next_cell = mob_navigation.path[0]
+		
+		var next_cell_pos_x = player_buildings.map_to_world(next_cell).x
+		var mob_x = global_position.x
+		
+		sprite.flip_h = next_cell_pos_x > mob_x
+#			sprite.flip_h = true
+#		else:
+#			sprite.flip_h = false
+#		print()
+#		sprite.flip_h = player_buildings.map_to_world(next_cell).x < 
+	
 
